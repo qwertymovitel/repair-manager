088712ff534afe8b99465b06100cf9d933973aef
qwertymovitel <110018134+qwertymovitel@users.noreply.github.com>
2026-03-16 11:35:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    # Change debug=False to debug=True temporarily
+    app.run(host='0.0.0.0', port=5000, debug=True)
